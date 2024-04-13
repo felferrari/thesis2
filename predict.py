38e9c7502119ea07c1d2b705e7648b79578db039
Felipe Ferrari <felferrari@gmail.com>
2024-04-13 14:43:52 -0300

    up

--- predict.py
+++ predict.py
@@ -21,20 +21,21 @@ def predict(cfg):
     parent_run_id = runs['run_id'][0]
 
     with mlflow.start_run(run_id=parent_run_id) as parent_run:
-        #data_module = DataModule(cfg)
-        
+
         with TemporaryDirectory() as tempdir:
             
             imgs_combinations = PredDataset.test_combinations(cfg)
-            for img_combination in imgs_combinations:
+            for img_comb_i, img_combination in enumerate(imgs_combinations):
+                
+                pred_sum = None
 
                 predict_dataset = PredDataset(cfg, img_combination)
                 gen_opt_imgs, gen_sar_imgs = predict_dataset.generate_sample_images(img_combination)
                 for opt_i, sample_opt in enumerate(gen_opt_imgs):
-                    mlflow.log_image(sample_opt, f'combination_{img_combination}/opt_{img_combination[0][opt_i]}.jpg')
+                    mlflow.log_image(sample_opt, f'combination_{img_comb_i}/opt_{img_combination[0][opt_i]}.jpg')
                 for sar_i, sample_sar in enumerate(gen_sar_imgs):
-                    mlflow.log_image(sample_sar[:,:,0], f'combination_{img_combination}/sar_0_{img_combination[1][sar_i]}.jpg')
-                    mlflow.log_image(sample_sar[:,:,1], f'combination_{img_combination}/sar_1_{img_combination[1][sar_i]}.jpg')
+                    mlflow.log_image(sample_sar[:,:,0], f'combination_{img_comb_i}/sar_0_{img_combination[1][sar_i]}.jpg')
+                    mlflow.log_image(sample_sar[:,:,1], f'combination_{img_comb_i}/sar_1_{img_combination[1][sar_i]}.jpg')
 
                 for model_i in range (cfg.general.n_models):
                     
@@ -42,13 +43,9 @@ def predict(cfg):
                         filter_string = f'run_name = "model_{model_i}" AND params.parent_run_id = "{parent_run_id}"'
                         )['run_id'][0]
                     
-                    with mlflow.start_run(run_id=run_model_id, nested=True):
+                    with mlflow.start_run(run_id=run_model_id, nested=True) as model_run:
                         model_id = f'runs:/{run_model_id}/model'
                         model_module = mlflow.pytorch.load_model(model_id)
-                        # model_module = ModelModule(cfg)
-                        # model_module_b = ModelModule(cfg)
-                        # model_module_b.model = model_module.model
-                        # model_module = model_module_b
                         
                         pred_callback = PredictionCallback(cfg)
                         callbacks = [pred_callback]
@@ -73,28 +70,34 @@ def predict(cfg):
                             return_predictions=False
                         )
                         pred = pred_callback.get_final_image()
-                        #pred_int = (pred*255.0).astype(np.uint8)
+                        
+                        if pred_sum is None:
+                            pred_sum = pred.copy()
+                        else:
+                            pred_sum += pred.copy()
+
                         base_image = Path(cfg.path.opt) / cfg.site.original_data.opt.train.imgs[0]
                         
                         elapsed_time = (time() - t0) / 60.0
                         
-                        pred_file_path= Path(tempdir) / f'{cfg.site.name}:{cfg.exp.name}:{img_combination}:{model_i}.tif'
-                        save_geotiff(base_image, pred_file_path, pred, 'float')
-                        # pred_0_file_path = Path(tempdir) / f'{cfg.site.name}:{cfg.exp.name}:{img_combination}:{model_i}_0.tif'
-                        # save_geotiff(base_image, pred_0_file_path, np.round(255.0*pred[:,:,0]).astype(np.uint8), 'byte')
-                        # pred_1_file_path = Path(tempdir) / f'{cfg.site.name}:{cfg.exp.name}:{img_combination}:{model_i}_1.tif'
-                        # save_geotiff(base_image, pred_1_file_path, np.round(255.0*pred[:,:,1]).astype(np.uint8), 'byte')
-                        
-                        #pred_callback.reset_image()
-                        
                         preview = rescale(pred[:,:,0:3], 0.1, channel_axis=2, preserve_range=True)
                         preview = 255 * preview 
                         preview = np.clip(preview.astype(np.int32), 0, 255)
-                        
+                        mlflow.log_image(preview, f'predictions/preview_{cfg.site.name}:{cfg.exp.name}:{img_comb_i}:{model_i}.jpg')
                         mlflow.log_metric('prediction_time', elapsed_time)
-                        mlflow.log_artifact(pred_file_path, 'predictions')
-                        #mlflow.log_artifact(pred_1_file_path, 'predictions')
-                        mlflow.log_image(preview, f'predictions/preview_{cfg.site.name}:{cfg.exp.name}:{img_combination}:{model_i}.jpg')
+
+                
+                avg_pred = pred_sum / cfg.general.n_models
+                pred_file_path= Path(tempdir) / f'{cfg.site.name}:{cfg.exp.name}:{img_comb_i}.tif'
+                save_geotiff(base_image, pred_file_path, avg_pred, 'float')
+                mlflow.log_artifact(pred_file_path, 'predictions')
+                
+                preview = rescale(avg_pred[:,:,0:3], 0.1, channel_axis=2, preserve_range=True)
+                preview = 255 * preview 
+                preview = np.clip(preview.astype(np.int32), 0, 255)
+                mlflow.log_image(preview, f'predictions/preview_{cfg.site.name}:{cfg.exp.name}:{img_comb_i}.jpg')
+                mlflow.log_metric('prediction_time', elapsed_time)
+                        
 
 if __name__ == "__main__":
     predict()
