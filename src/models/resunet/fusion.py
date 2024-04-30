0585c529755a3d6f0f89a47d2d99b58b9c32909d
Felipe Ferrari <felferrari@gmail.com>
2024-04-30 09:49:50 -0300

    up

---
+++ src/models/resunet/fusion.py
@@ -0,0 +1,13 @@
+from .single import GenericResunet
+from abc import abstractmethod
+
+class PixelLevel(GenericResunet):
+    @abstractmethod
+    def get_input_dims(cfg):
+        return cfg.exp.n_opt_imgs * cfg.general.n_opt_bands + cfg.exp.n_sar_imgs * cfg.general.n_sar_bands + 1 
+    
+    def prepare(self, x):
+        x_opt = self.get_opt(x)
+        x_sar = self.get_sar(x)
+        #x = torch.cat((x_img, x['previous']), dim=1)
+        return (x_opt, x_sar, x['previous'])
