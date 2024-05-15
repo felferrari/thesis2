1ea2c99c4d798ab577923b336d584bdd29962c7f
Felipe Ferrari <felferrari@gmail.com>
2024-05-15 07:56:48 -0300

    up

--- run_all.sh
+++ run_all.sh
@@ -1,8 +1,18 @@
-python train.py +exp=exp_3 +site=s1
-python predict.py +exp=exp_3 +site=s1
+python train.py +exp=exp_412 +site=s1
+python predict.py +exp=exp_412 +site=s1
+python evaluate.py +exp=exp_412 +site=s1
 
-python train.py +exp=exp_1 +site=s1
-python predict.py +exp=exp_1 +site=s1
+python train.py +exp=exp_461 +site=s1
+python predict.py +exp=exp_461 +site=s1
+python evaluate.py +exp=exp_461 +site=s1
 
-python train.py +exp=exp_2 +site=s1
-python predict.py +exp=exp_2 +site=s1
+python train.py +exp=exp_462 +site=s1
+python predict.py +exp=exp_462 +site=s1
+python evaluate.py +exp=exp_462 +site=s1
+
+python train.py +exp=exp_411 +site=s1
+python predict.py +exp=exp_411 +site=s1
+python evaluate.py +exp=exp_411 +site=s1
+
+
+
